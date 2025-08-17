ab2f398beee01bc11c7938d76561ac13035006e5
sn4yber <snayber333@gmail.com>
2025-08-17 22:19:02 +0000

    imagen

--- sn4yber-react-portfolio/src/pages/Projects.tsx
+++ sn4yber-react-portfolio/src/pages/Projects.tsx
@@ -65,7 +65,7 @@ export const Projects = () => {
       category: 'e-commerce de venta de bikinis',
       title: 'Tienda de Bikinis',
       description: 'sistema e-commerce de venta de bikinis donde se hace la gestion de productos, carrito de compras y pagos en linea con una interfaz amigable y moderna.',
-      image: '/ecommerce.png',
+      image: '/ecomerse.png',
       features: ['E-commerce', 'Modern', 'Responsive' ,'ventas en linea ' ,],
       technologies: ['React', 'Node.js', 'PostgreSQL', 'Tailwind CSS' , 'typeScript' , 'prisma'],
       links: {
