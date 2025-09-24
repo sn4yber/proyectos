ec111322a0acb814f938cf7dd2604a8f86372818
sn4yber <snayber333@gmail.com>
2025-09-24 15:49:03 +0000

    deploy

--- sn4yber-react-portfolio/src/components/ProjectCard3D/ProjectCard3D.tsx
+++ sn4yber-react-portfolio/src/components/ProjectCard3D/ProjectCard3D.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import { memo } from 'react'
 import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion'
 import { useRef, MouseEvent } from 'react'
 import { ExternalLink, Github } from 'lucide-react'
